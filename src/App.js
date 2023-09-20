235c5c151ed1ec180396c1779cee56c897ab4382
Kazeem Amuzat Asojuomode <77699947+amuzatk@users.noreply.github.com>
2023-09-20 19:51:14 +0100

    new branch created

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Find from "./components/Find/Find";
-import Video from './components/Video'
 import Driver from "./components/Driver/Driver";
 import Luxury from "./components/Luxury/Luxury";
 import Footer from "./components/Footer/Footer";
